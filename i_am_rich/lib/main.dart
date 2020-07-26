233af085a4cd9509e5f2e57e5ab745bf54ffe34b
RuoqingVeronicaGan <ganruoqing1995@gmail.com>
2020-07-25 17:01:59 -0700

    'Hello World!'

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+// The main function is the starting point for all our Flutter apps.
 void main() {
-  runApp(MaterialApp());
-  
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World!'),
+      ),
+    ),
+  );
 }
-
