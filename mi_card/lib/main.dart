551fd44f9704a9baff4b3df20b009d70db92ad91
Ruoqing <ganruoqing1995@gmail.com>
2020-08-01 22:53:36 -0700

    practiced using columns and rows widgets.

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -2,12 +2,44 @@ import 'package:flutter/material.dart';
 
 // test commit
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                height: 100.0,
+                width: 100.0,
+                child: Text("Container 1"),
+                padding: EdgeInsets.all(25),
+              ),
+              Container(
+                color: Colors.pink,
+                height: 100.0,
+                width: 100.0,
+                child: Text("Container 2"),
+                padding: EdgeInsets.all(25),
+              ),
+              Container(
+                color: Colors.grey[700],
+                height: 100.0,
+                width: 100.0,
+                child: Text("Container 3"),
+                padding: EdgeInsets.all(25),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
