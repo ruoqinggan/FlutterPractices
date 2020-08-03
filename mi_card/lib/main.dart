49d6aa7f53423778dccdf9812b8cf1b043072710
Ruoqing <ganruoqing1995@gmail.com>
2020-08-02 17:31:46 -0700

    practiced using Stateless and more layout widgets.

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,36 +10,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.lightGreen,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                height: double.infinity,
-                width: 100.0,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/ruoqing.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    height: 100.0,
-                    width: 100.0,
+              Text(
+                'Ruoqing Gan',
+                style: TextStyle(
+                  fontFamily: 'ArchitectsDaughter',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 2.0,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 6.5,
+                  fontSize: 20.0,
+                  color: Colors.lightGreen.shade50,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.lime[500],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 35.0,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_in_talk,
+                    size: 25.0,
+                    color: Colors.lime[600],
                   ),
-                  Container(
-                    color: Colors.green,
-                    height: 100.0,
-                    width: 100.0,
+                  title: Text(
+                    '(650) 772 1006',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      letterSpacing: 1.0,
+                      fontSize: 20.0,
+                    ),
                   ),
-                ],
+                ),
               ),
-              Container(
-                color: Colors.blue,
-                height: double.infinity,
-                width: 100.0,
-              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 35.0,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 25.0,
+                    color: Colors.lime[600],
+                  ),
+                  title: Text(
+                    'ganruoqing1995@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
