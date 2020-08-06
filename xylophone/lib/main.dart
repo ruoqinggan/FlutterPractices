a659a3694e4107fb5d49be3c3e8849f99b6fa12e
Ruoqing <ganruoqing1995@gmail.com>
2020-08-05 23:20:54 -0700

    used functions to simplify code.

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNum) {
+    final player = AudioCache();
+    player.play('note$noteNum.wav');
+  }
+
+  Expanded buildKey(int noteNum, Color color) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(noteNum);
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.blue),
+              buildKey(6, Colors.indigo),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
