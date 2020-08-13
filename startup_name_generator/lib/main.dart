0f55bcd54ae7ba395ed7839427c74038eab8cdc6
Ruoqing <ganruoqing1995@gmail.com>
2020-08-13 01:30:19 -0700

    made list of randomly generated names.

--- startup_name_generator/lib/main.dart
+++ startup_name_generator/lib/main.dart
@@ -1,117 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// Arrow notation => is for one-line functions or methods.
+void main() => runApp(MyApp());
 
+// The app extends StatelessWidget, which makes the app itself a widget.
+// In Flutter, almost everything is a widget, including alignment, padding,
+// and layout.
+// Stateless widgets are immutable, meaning that their properties can't
+// change - all values are final.
+//
+// Stateful widgets maintain state that might change during the lifetime
+// of the widget. Implementing a stateful widget requires at least two
+// classes: a StatefulWidget object that creates an instance of a State
+// class. The StatefulWidget object is, itself, immutable and can be
+// thrown away and regenerated, but the State object persists over the
+// lifetime of the widget.
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // A widget's main job is to provide a build() method that describes
+  // how to display the widget in terms of other, low-level widgets.
   @override
   Widget build(BuildContext context) {
+    // This example creates a Material app. Material is a visual-design
+    // language that's standard on mobile and the web. Flutter offers a
+    // rich set of Material widgets.
     return MaterialApp(
-      title: 'Hello world',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Startup Name Generator',
+
+      
+      home: RandomWords(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+// This is a stateful widget, RandomWords, which creates its State class,
+// _RandomWordsState. RandomWords is used as a child inside the existing
+// MyApp stateless widget.
+class RandomWords extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// By default, the name of the State class is prefixed with an underscore.
+// Prefixing an identifier with an undersore enforces privacy in the Dart
+// language and is a recommended best practice for State objects.
+//
+// The IDE also automatically updates the state class to extend
+// State<RandomWords>, indicating that you're using a generic State
+// class specialized for use with RandomWords. Most of the app's logic
+// resides here - it maintains the state for teh RandomWords widget.
+//
+// The class saves the list of generated word pairs, which grows infinitely
+// as the user scorlls.
+class _RandomWordsState extends State<RandomWords> {
+  // A _suggestions list for saving suggested word pairings.
+  final List<WordPair> _suggestions = <WordPair>[];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // A _biggerFont variable for making the font size larger.
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return Scaffold (                     // Add from here... 
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Startup Name Generator'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: _buildSuggestions(),
+    ); 
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+
+      // The itemBuilder callback is called once per suggested
+      // word pairing, and places each suggestion into a ListTile
+      // row. For even rows, the function adds a ListTile row for
+      // the word pairing. For odd rows, the fuction adds a Divider
+      // widget to visually separate the entries. Note that the
+      // divider may be difficult to see on smaller devices.
+      itemBuilder: (BuildContext _context, int i) {
+        // Add a one-pixel high divider widget before each row
+        // in the ListView.
+        if (i.isOdd) {
+          return Divider(
+            color: Colors.indigo[100],
+          );
+        }
+
+        // The syntax "i ~/ 2" divides i by 2 and returns an
+        // integer result.
+        // For example, 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        // This caculates the actual number of word pairings
+        // in the ListView, minus the divider widgets.
+        final int index = i ~/ 2;
+
+        // If you've reached the end of the available word
+        // parings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the
+          // suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
