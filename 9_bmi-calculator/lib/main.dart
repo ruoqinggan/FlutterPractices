cc20c2917b2391bae8c11dd3f96d9505943e6522
Ruoqing <ganruoqing1995@gmail.com>
2020-08-13 02:13:41 -0700

    customized theme colors.

--- 9_bmi-calculator/lib/main.dart
+++ 9_bmi-calculator/lib/main.dart
@@ -6,6 +6,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        accentColor: Colors.amber,
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(body1: TextStyle(color: Color(0xFFFFFFFF))),
+      ),
       home: InputPage(),
     );
   }
