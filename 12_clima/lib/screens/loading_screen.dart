acbc3852ac017a79a208394d0857862f9dc944a7
Ruoqing <ganruoqing1995@gmail.com>
2020-08-27 22:49:06 -0700

    used http package to get weather info from API

--- 12_clima/lib/screens/loading_screen.dart
+++ 12_clima/lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,6 +20,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await currentLocation.getCurrentLocation();
     print(currentLocation.latitude);
     print(currentLocation.longitude);
+    getWeatherData();
+  }
+
+  void getWeatherData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+
+    print(response.body);
   }
 
   @override
