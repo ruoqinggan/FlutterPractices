d782ec0ed07f9340e4431e81b2d1f115d2ff412e
Ruoqing <ganruoqing1995@gmail.com>
2020-08-28 19:53:47 -0700

    updated networking.dart

--- 12_clima/lib/services/networking.dart
+++ 12_clima/lib/services/networking.dart
@@ -1,1 +1,20 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  final String url;
+
+  NetworkHelper(this.url);
+
+  Future getData() async {
+    http.Response response = await http.get(url);
+
+    // if http request was successful
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return json.decode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
