2d42f3bfe3f50ef393cc9993836a69b6138de5b3
Ruoqing <ganruoqing1995@gmail.com>
2020-08-31 02:36:05 -0700

    added navigation to city screen.

--- 12_clima/lib/screens/loading_screen.dart
+++ 12_clima/lib/screens/loading_screen.dart
@@ -1,13 +1,7 @@
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
-import '../services/location.dart';
-import '../services/networking.dart';
-import '../screens/location_screen.dart';
 import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = '06cf79b723b8399c2630af3283a4af0b';
-const unit = 'metric';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,13 +16,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=$unit');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getWeatherByLocation();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
