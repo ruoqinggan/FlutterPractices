edd7e156994daa749d298f90713052073e8913f0
Ruoqing <ganruoqing1995@gmail.com>
2020-08-31 11:59:05 -0700

    moved input textfield style to constants.dart

--- 12_clima/lib/screens/city_screen.dart
+++ 12_clima/lib/screens/city_screen.dart
@@ -37,24 +37,10 @@ class _CityScreenState extends State<CityScreen> {
                   style: TextStyle(
                     color: Colors.black,
                   ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    icon: Icon(
-                      Icons.location_city,
-                      color: Colors.white,
-                    ),
-                    hintText: 'Enter City Name',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
-                      ),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    print(value);
+                  },
                 ),
               ),
               FlatButton(
